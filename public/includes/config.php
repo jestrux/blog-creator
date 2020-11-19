b2d3d15d8641eedfb9c76eb145059225a978b747
Walter Kimaro <wakyj07@gmail.com>
2020-11-19 15:52:06 +0300

    Update to latest local configs

--- public/includes/config.php
+++ public/includes/config.php
@@ -11,7 +11,8 @@
 	defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 	defined('DB_USER')   ? null : define("DB_USER", "root");
 	defined('DB_PASS')   ? null : define("DB_PASS", "root");
-	defined('DB_PORT')   ? null : define("DB_PORT", "8889");
-	defined('DB_SOCKET') ? null : define("DB_SOCKET", "/Applications/MAMP/tmp/mysql/mysql.sock");
-	defined('DB_NAME')   ? null : define("DB_NAME", "mkawa");
+	defined('DB_PORT')   ? null : define("DB_PORT", "3306");
+	// defined('DB_SOCKET') ? null : define("DB_SOCKET", "/Applications/MAMP/tmp/mysql/mysql.sock");
+	defined('DB_SOCKET') ? null : define("DB_SOCKET", "/tmp/mysql.sock");
+	defined('DB_NAME')   ? null : define("DB_NAME", "youlead");
 ?>
